3cc183fd08b006f135d30f722809f483b1416da6
anarcheril <anarcheril@gmail.com>
2021-03-05 17:38:39 -0600

    Update ByteConverter.cs
    Cleaned up code a bit (potentially faster?), and added get and set float

--- ByteConverter.cs
+++ ByteConverter.cs
@@ -1,74 +1,60 @@
 public static class ByteConverter
 {
-    public static unsafe int GetInt(byte[] bytes, int index)
+    public static unsafe float GetFloat(byte[] bytes, uint index)
     {
-        int retVal;
         fixed (byte* pbytes = &bytes[index])
-        {
-            retVal = *(int*)pbytes;
-        }
-        return retVal;
+            return *(float*)pbytes;
     }
-    public static unsafe void SetInt(int inVal, byte[] bytes, int index)
+    public static unsafe void SetFloat(float inVal, byte[] bytes, uint index)
+    {
+        fixed (byte* pbytes = &bytes[index])
+            *(float*)pbytes = inVal;
+    }
+    public static unsafe int GetInt(byte[] bytes, uint index)
+    {
+        fixed (byte* pbytes = &bytes[index])
+            return *(int*)pbytes;
+    }
+    public static unsafe void SetInt(int inVal, byte[] bytes, uint index)
     {
         fixed(byte* pbytes = &bytes[index])
-        {
             *(int*)pbytes = inVal;
-        }
     }
-    public static unsafe uint GetUint(byte[] bytes, int index)
-    {
-        uint retVal;
-        fixed (byte* pbytes = &bytes[index])
-        {
-            retVal = *(uint*)pbytes;
-        }
-        return retVal;
-    }
-    public static unsafe void SetUint(uint inVal, byte[] bytes, int index)
+    public static unsafe uint GetUint(byte[] bytes, uint index)
     {
         fixed (byte* pbytes = &bytes[index])
-        {
+            return *(uint*)pbytes;
+    }
+    public static unsafe void SetUint(uint inVal, byte[] bytes, uint index)
+    {
+        fixed (byte* pbytes = &bytes[index])
             *(uint*)pbytes = inVal;
-        }
     }
-    public static unsafe ushort GetUshort(byte[] bytes, int index)
+    public static unsafe ushort GetUshort(byte[] bytes, uint index)
     {
-        ushort retVal;
         fixed (byte* pbytes = &bytes[index])
-        {
-            retVal = *(ushort*)pbytes;
-        }
-        return retVal;
+            return *(ushort*)pbytes;
     }
-    public static unsafe void SetShort(short inVal, byte[] bytes, int index)
+    public static unsafe void SetShort(short inVal, byte[] bytes, uint index)
     {
         fixed(byte* pbytes = &bytes[index])
-        {
             *(short*)pbytes = inVal;
-        }
     }
-    public static unsafe short GetShort(byte[] bytes, int index)
-    {
-        short retVal;
-        fixed (byte* pbytes = &bytes[index])
-        {
-            retVal = *(short*)pbytes;
-        }
-        return retVal;
-    }
-    public static unsafe void SetUshort(ushort inVal, byte[] bytes, int index)
+    public static unsafe short GetShort(byte[] bytes, uint index)
     {
         fixed (byte* pbytes = &bytes[index])
-        {
+            return *(short*)pbytes;
+    }
+    public static unsafe void SetUshort(ushort inVal, byte[] bytes, uint index)
+    {
+        fixed (byte* pbytes = &bytes[index])
             *(ushort*)pbytes = inVal;
-        }
     }
-    public static unsafe void setByte(byte inVal, byte[] bytes, int index)
+    public static unsafe void SetByte(byte inVal, byte[] bytes, uint index)
     {
         bytes[index] = inVal;
     }
-    public static unsafe byte getByte(byte[] bytes, int index)
+    public static unsafe byte GetByte(byte[] bytes, uint index)
     {
         return bytes[index];
     }
